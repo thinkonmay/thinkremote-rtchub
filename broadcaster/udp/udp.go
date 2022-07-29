8bbe7553df4988f1e231fedab9d070a1672a86ac
pigeatgarlic <huyhoangdo0205@gmail.com>
2022-07-29 10:53:40 +0700

    multiple write loop

--- broadcaster/udp/udp.go
+++ broadcaster/udp/udp.go
@@ -18,7 +18,44 @@ type UDPBroadcaster struct {
 	bufferSize int
 
 	closeChannel chan bool
+	packetChannel chan *rtp.Packet
 }
+
+type loop func ();
+
+type writeLoop struct {
+	conn net.Conn
+	chann *chan *rtp.Packet
+
+
+	lop loop
+	buf []byte
+}
+
+func newloop(conn net.Conn, channel chan *rtp.Packet) ( writeloop *writeLoop ) {
+	writeloop = &writeLoop{}
+	writeloop.conn = conn
+	writeloop.chann = &channel;
+	writeloop.buf = make([]byte, 10000);
+	return;
+}
+
+func (loop *writeLoop)runloop(){
+	loop.lop = func() {
+		packet := <- *loop.chann;
+		size, err := packet.MarshalTo(loop.buf)
+		if err != nil {
+			fmt.Printf("%v", err)
+		}
+		_,err = loop.conn.Write(loop.buf[:size]);
+		if err != nil {
+			fmt.Printf("%s\n",err.Error());	
+		}
+	};
+	go loop.lop();
+}
+
+
 
 
 func NewUDPBroadcaster(config *config.BroadcasterConfig) (udp *UDPBroadcaster, err error) {
@@ -31,23 +68,20 @@ func NewUDPBroadcaster(config *config.BroadcasterConfig) (udp *UDPBroadcaster, e
 	}
 	udp.buffer = make([]byte, udp.bufferSize);
 	udp.closeChannel = make(chan bool);
+	udp.packetChannel = make(chan *rtp.Packet);
 	udp.conn,err = net.Dial("udp",fmt.Sprintf("localhost:%d",config.Port));
+
+
+	newloop(udp.conn,udp.packetChannel).runloop();
+	newloop(udp.conn,udp.packetChannel).runloop();
+	newloop(udp.conn,udp.packetChannel).runloop();
 	return;
 }
 
 
 
 func (udp *UDPBroadcaster) Write(packet *rtp.Packet) {
-	size, err := packet.MarshalTo(udp.buffer)
-	if err != nil {
-		fmt.Printf("%v", err)
-	}
-
-
-	_,err = udp.conn.Write(udp.buffer[:size]);
-	if err != nil {
-		fmt.Printf("%s\n",err.Error());	
-	}
+	udp.packetChannel <- packet;
 }
 
 func (udp *UDPBroadcaster)	Close() {
