2d6d4f2be1ac44ca460f99a577705ff757597f5e
pigeatgarlic <huyhoangdo0205@gmail.com>
2022-12-20 06:24:25 -0800

    fix ref frame

--- util/test/test.go
+++ util/test/test.go
@@ -185,7 +185,7 @@ func GstTestMediaFoundation(source *tool.Monitor) (string, float64) {
 		"!", "queue", "max-size-time=0", "max-size-bytes=0", "max-size-buffers=3", "!",
 		"d3d11convert",
 		"!", "queue", "max-size-time=0", "max-size-bytes=0", "max-size-buffers=3", "!",
-		"mfh264enc", fmt.Sprintf("bitrate=%d", defaultVideoBitrate), "rc-mode=0", "low-latency=true", "ref=0", "quality-vs-speed=0", "name=encoder",
+		"mfh264enc", fmt.Sprintf("bitrate=%d", defaultVideoBitrate), "rc-mode=0", "low-latency=true", "ref=1", "quality-vs-speed=0", "name=encoder",
 		"!", "queue", "max-size-time=0", "max-size-bytes=0", "max-size-buffers=3", "!",
 		"appsink", "name=appsink")
 
