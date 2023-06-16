5ad87c1efeb014330327d33bccb50ed3c644c40e
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-06-16 11:11:24 +0700

    init feature

--- listener/audio/audio.go
+++ listener/audio/audio.go
@@ -13,9 +13,152 @@ import (
 	"github.com/thinkonmay/thinkremote-rtchub/listener/rtppay/opus"
 )
 
-// #cgo LDFLAGS: ${SRCDIR}/../../cgo/lib/libshared.a
-// #cgo pkg-config: gstreamer-1.0 gstreamer-app-1.0
-// #include "webrtc_audio.h"
+/*
+
+#include <gst/app/gstappsrc.h>
+
+void PushBufferAudio	   (void  *buffer, int bufferLen , int samples) {
+
+}
+
+int  PopBufferAudio	   	  	(void **buffer, int *samples) {
+	return 0;
+}
+
+void handleAudioStopOrError() {
+
+}
+
+
+
+
+
+
+void*         create_audio_pipeline     (char *pipeline,
+                                         char** err);
+
+void          audio_pipeline_set_bitrate(void* pipeline,
+                                         int bitrate);
+
+void          start_audio_pipeline      (void* pipeline);
+
+void          stop_audio_pipeline       (void* pipeline);
+
+void          start_audio_mainloop      (void);
+
+void*         set_media_device();
+
+
+
+GMainLoop *gstreamer_audio_main_loop = NULL;
+void
+start_audio_mainloop(void) {
+    gstreamer_audio_main_loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(gstreamer_audio_main_loop);
+}
+
+
+
+
+static gboolean
+handle_gstreamer_bus_call(GstBus *bus, GstMessage *msg, gpointer data) {
+    switch (GST_MESSAGE_TYPE(msg)) {
+
+    case GST_MESSAGE_EOS:
+        g_print("End of stream\n");
+        handleAudioStopOrError();
+        break;
+
+    case GST_MESSAGE_ERROR: {
+        gchar *debug;
+        GError *error;
+        gst_message_parse_error(msg, &error, &debug);
+        g_free(debug);
+        g_printerr("Audio pipeline error: %s\n", error->message);
+        g_error_free(error);
+        handleAudioStopOrError();
+    }
+    default:
+        break;
+    }
+
+    return TRUE;
+}
+
+GstFlowReturn
+handle_audio_buffer(GstElement *object, gpointer user_data) {
+    GstSample *sample = NULL;
+    GstBuffer *buffer = NULL;
+    gsize copy_size = 0;
+
+    g_signal_emit_by_name (object, "pull-sample", &sample);
+    if (sample) {
+        buffer = gst_sample_get_buffer(sample);
+        if (buffer) {
+            copy_size = gst_buffer_get_size(buffer);
+            if(copy_size) {
+                gpointer copy = malloc(copy_size);
+                gst_buffer_extract(buffer, 0, (gpointer)copy, copy_size);
+                PushBufferAudio(copy, copy_size, GST_BUFFER_DURATION(buffer));
+                free(copy);
+            }
+        }
+        gst_sample_unref (sample);
+    }
+
+    return GST_FLOW_OK;
+}
+
+void*
+create_audio_pipeline(char *pipeline,
+                      char** err)
+{
+    gst_init(NULL, NULL);
+
+    *err = NULL;
+    GError *error = NULL;
+    GstElement * ret = gst_parse_launch(pipeline, &error);
+    if (error) {
+        *err = error->message;
+        return NULL;
+    }
+    return ret;
+}
+
+void
+start_audio_pipeline(void* pipeline)
+{
+    GstBus *bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
+    gst_bus_add_watch(bus, handle_gstreamer_bus_call, pipeline);
+    gst_object_unref(bus);
+
+
+    GstElement *appsink = gst_bin_get_by_name(GST_BIN(pipeline), "appsink");
+    g_object_set(appsink, "emit-signals", TRUE, NULL);
+    g_signal_connect(appsink, "new-sample", G_CALLBACK(handle_audio_buffer), NULL);
+    gst_object_unref(appsink);
+
+    gst_element_set_state((GstElement*)pipeline, GST_STATE_PLAYING);
+}
+
+void
+audio_pipeline_set_bitrate(void* pipeline, int bitrate) {
+    GstElement *encoder = gst_bin_get_by_name(GST_BIN(pipeline), "encoder");
+    g_object_set(encoder, "bitrate", bitrate, NULL);
+}
+
+void
+stop_audio_pipeline(void* pipeline) {
+    gst_element_set_state((GstElement*)pipeline, GST_STATE_NULL);
+}
+
+int
+string_get_length(void* string)
+{
+    return strlen((char*)string);
+}
+#cgo pkg-config: gstreamer-1.0 gstreamer-app-1.0
+*/
 import "C"
 
 func init() {
@@ -24,13 +167,13 @@ func init() {
 
 // Pipeline is a wrapper for a GStreamer Pipeline
 type Pipeline struct {
-	closed     bool
+	closed      bool
 	pipeline    unsafe.Pointer
 	pipelineStr string
 
 	clockRate float64
 
-	codec      string
+	codec string
 
 	Multiplexer *multiplexer.Multiplexer
 }
@@ -38,14 +181,14 @@ type Pipeline struct {
 var pipeline *Pipeline
 
 // CreatePipeline creates a GStreamer Pipeline
-func CreatePipeline(pipelinestr string) (*Pipeline,error) {
+func CreatePipeline(pipelinestr string) (*Pipeline, error) {
 	pipeline = &Pipeline{
-		pipeline:     unsafe.Pointer(nil),
-		pipelineStr:  pipelinestr,
-		clockRate:    48000,
-		codec:        webrtc.MimeTypeOpus,
+		pipeline:    unsafe.Pointer(nil),
+		pipelineStr: pipelinestr,
+		clockRate:   48000,
+		codec:       webrtc.MimeTypeOpus,
 
-		Multiplexer: multiplexer.NewMultiplexer("audio",func() rtppay.Packetizer {
+		Multiplexer: multiplexer.NewMultiplexer("audio", func() rtppay.Packetizer {
 			return opus.NewOpusPayloader()
 		}),
 	}
@@ -53,34 +196,31 @@ func CreatePipeline(pipelinestr string) (*Pipeline,error) {
 	pipelineStrUnsafe := C.CString(pipeline.pipelineStr)
 	defer C.free(unsafe.Pointer(pipelineStrUnsafe))
 
-	var err unsafe.Pointer
-	fmt.Printf("starting audio pipeline %s\n",pipelinestr)
+	err := C.CString("")
+	fmt.Printf("starting audio pipeline %s\n", pipelinestr)
 	pipeline.pipeline = C.create_audio_pipeline(pipelineStrUnsafe, &err)
-	err_str := ToGoString(err)
+	err_str := C.GoString(err)
 	if len(err_str) != 0 {
-		return nil,fmt.Errorf("fail to create pipeline %s",err_str);
+		return nil, fmt.Errorf("fail to create pipeline %s", err_str)
 	}
 
-
-	return pipeline,nil
+	return pipeline, nil
 }
 
-//export goHandlePipelineBufferAudio
-func goHandlePipelineBufferAudio(buffer unsafe.Pointer, bufferLen C.int, duration C.int) {
+func BufferAudio(buffer unsafe.Pointer, bufferLen C.int, duration C.int) {
 	samples := uint32(time.Duration(duration).Seconds() * pipeline.clockRate)
-	pipeline.Multiplexer.Send(buffer, uint32(bufferLen), uint32(samples) )
+	pipeline.Multiplexer.Send(buffer, uint32(bufferLen), uint32(samples))
 }
 
-//export handleAudioStopOrError
 func handleAudioStopOrError() {
 	pipeline.Close()
 
-	var err unsafe.Pointer
+	err := C.CString("")
 	pipelineStrUnsafe := C.CString(pipeline.pipelineStr)
 	pipeline.pipeline = C.create_audio_pipeline(pipelineStrUnsafe, &err)
-	err_str := ToGoString(err)
+	err_str := C.GoString(err)
 	if len(err_str) != 0 {
-		fmt.Printf("fail to create pipeline %s",err_str);
+		fmt.Printf("fail to create pipeline %s", err_str)
 	}
 
 	pipeline.Open()
@@ -94,7 +234,6 @@ func (p *Pipeline) Open() {
 	fmt.Println("starting audio pipeline")
 	C.start_audio_pipeline(pipeline.pipeline)
 }
-
 
 func (p *Pipeline) Close() {
 	fmt.Println("stoping audio pipeline")
@@ -111,18 +250,9 @@ func (p *Pipeline) SetProperty(name string, val int) error {
 	return fmt.Errorf("unknown prop")
 }
 
-func ToGoString(str unsafe.Pointer) string {
-	if str == nil {
-		return ""
-	}
-	return string(C.GoBytes(str, C.int(C.string_get_length(str))))
-}
-
-
-
 
 func (p *Pipeline) RegisterRTPHandler(id string, fun func(pkt *rtp.Packet)) {
-	p.Multiplexer.RegisterRTPHandler(id,fun)
+	p.Multiplexer.RegisterRTPHandler(id, fun)
 }
 
 func (p *Pipeline) DeregisterRTPHandler(id string) {
--- listener/video/video.go
+++ listener/video/video.go
@@ -1,6 +1,190 @@
 // Package gst provides an easy API to create an appsink pipeline
 package video
 
+/*
+
+#include <gst/app/gstappsrc.h>
+#include <gst/video/video-event.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <glib.h>
+#include <pthread.h>
+
+void PushBufferVideo	   (void  *buffer, int bufferLen , int samples) {
+
+}
+
+int  PopBufferVideo	   				   (void **buffer, int *samples) {
+	return 0;
+}
+
+
+
+
+void handleVideoStopOrError            () {
+
+}
+
+void*create_video_pipeline(char *pipeline,char** err);
+void video_pipeline_set_bitrate(void* pipeline, int bitrate);
+void video_pipeline_set_framerate(void* pipeline, int framerate);
+void start_video_pipeline(void*pipeline);
+void stop_video_pipeline(void*pipeline);
+void force_gen_idr_frame_video_pipeline(void*pipeline);
+void start_video_mainloop(void);
+
+
+GMainLoop *gstreamer_video_main_loop = NULL;
+void
+start_video_mainloop(void) {
+    gstreamer_video_main_loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(gstreamer_video_main_loop);
+}
+
+static gboolean
+gstreamer_send_video_bus_call(GstBus *bus, GstMessage *msg, gpointer data) {
+    switch (GST_MESSAGE_TYPE(msg)) {
+
+    case GST_MESSAGE_EOS:
+        g_print("End of stream\n");
+        handleVideoStopOrError();
+        break;
+
+    case GST_MESSAGE_ERROR: {
+        gchar *debug;
+        GError *error;
+
+        gst_message_parse_error(msg, &error, &debug);
+        g_free(debug);
+
+        g_printerr("Video pipeline error: %s\n", error->message);
+        g_error_free(error);
+        handleVideoStopOrError();
+    }
+    default:
+        break;
+    }
+
+    return TRUE;
+}
+
+GstFlowReturn
+handle_video_sample(GstElement *object, gpointer user_data) {
+    GstSample *sample = NULL;
+    GstBuffer *buffer = NULL;
+    gsize copy_size = 0;
+
+    g_signal_emit_by_name (object, "pull-sample", &sample);
+    if (sample) {
+        buffer = gst_sample_get_buffer(sample);
+        if (buffer) {
+            copy_size = gst_buffer_get_size(buffer);
+            if(copy_size) {
+                gpointer copy = malloc(copy_size);
+                gst_buffer_extract(buffer, 0, (gpointer)copy, copy_size); // linking gstreamer to go limited available stack frame // very dangerous to modify
+                PushBufferVideo(copy, copy_size, GST_BUFFER_DURATION(buffer));
+                free(copy);
+            }
+        }
+        gst_sample_unref (sample);
+    }
+    return GST_FLOW_OK;
+}
+
+void*
+create_video_pipeline(char *pipeline,
+                      char** err) {
+    if (!pipeline) {
+        *err = (void*)"empty pipeline";
+        return NULL;
+    }
+
+    gst_init(NULL, NULL);
+
+    *err = NULL;
+    GError *error = NULL;
+    GstElement* el = gst_parse_launch(pipeline, &error);
+    if (error) {
+        *err = error->message;
+        return NULL;
+    }
+
+    return (void*)el;
+}
+
+void
+start_video_pipeline(void* pipeline) {
+    if (!pipeline)
+        return;
+
+    GstBus *bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
+    gst_bus_add_watch(bus, gstreamer_send_video_bus_call, NULL);
+    gst_object_unref(bus);
+
+    GstElement *appsink = gst_bin_get_by_name(GST_BIN(pipeline), "appsink");
+    g_object_set(appsink, "emit-signals", TRUE, NULL);
+    g_signal_connect(appsink, "new-sample", G_CALLBACK(handle_video_sample), NULL);
+    gst_object_unref(appsink);
+
+    gst_element_set_state((GstElement*)pipeline, GST_STATE_PLAYING);
+}
+
+
+void
+video_pipeline_set_framerate(void* pipeline, int framerate) {
+    if (!pipeline)
+        return;
+
+    GstElement *framerateFilter = gst_bin_get_by_name(GST_BIN(pipeline), "framerateFilter");
+    if (!framerateFilter)
+        return;
+
+    char* capsstr = g_strdup_printf ("video/x-raw(memory:D3D11Memory),framerate=%d/1",framerate);
+    GstCaps* caps = gst_caps_from_string (capsstr);
+    g_free (capsstr);
+
+    g_object_set (framerateFilter, "caps", caps, NULL);
+    gst_caps_unref (caps);
+}
+
+
+void
+video_pipeline_set_bitrate(void* pipeline, int bitrate) {
+    if (!pipeline)
+        return;
+
+    GstElement *encoder = gst_bin_get_by_name(GST_BIN(pipeline), "encoder");
+    g_object_set(encoder, "bitrate", bitrate, NULL);
+    g_object_set(encoder, "gop-size", 0, NULL);
+}
+
+void
+stop_video_pipeline(void* pipeline) {
+    if (!pipeline)
+        return;
+
+    gst_element_set_state((GstElement*)pipeline, GST_STATE_NULL);
+}
+void
+force_gen_idr_frame_video_pipeline(void* pipeline) {
+    if (!pipeline)
+        return;
+
+    GstElement *encoder = gst_bin_get_by_name(GST_BIN(pipeline), "encoder");
+    if (!encoder)
+        return;
+
+    GstPad* srcpad = gst_element_get_static_pad(encoder,"src");
+
+	   GstEvent* event = gst_video_event_new_upstream_force_key_unit(GST_CLOCK_TIME_NONE,TRUE,0);
+    gst_pad_send_event(srcpad,event);
+    gst_object_unref(srcpad);
+}
+
+#include <stdlib.h>
+#cgo pkg-config: gstreamer-1.0 gstreamer-app-1.0 gstreamer-video-1.0
+*/
+import "C"
 import (
 	"fmt"
 	"time"
@@ -15,16 +199,9 @@ import (
 	"github.com/thinkonmay/thinkremote-rtchub/listener/video/adaptive"
 )
 
-// #cgo pkg-config: gstreamer-1.0 gstreamer-app-1.0 gstreamer-video-1.0
-// #cgo LDFLAGS: ${SRCDIR}/../../cgo/lib/libshared.a
-// #include "webrtc_video.h"
-import "C"
-
 func init() {
 	go C.start_video_mainloop()
 }
-
-
 
 // Pipeline is a wrapper for a GStreamer Pipeline
 type Pipeline struct {
@@ -35,63 +212,58 @@ type Pipeline struct {
 	pipelineStr string
 	clockRate   float64
 
-
 	Multiplexer *multiplexer.Multiplexer
 
-	codec      string
+	codec string
 
-	AdsContext     datachannel.DatachannelConsumer
+	AdsContext datachannel.DatachannelConsumer
 }
 
 var pipeline *Pipeline
 
 // CreatePipeline creates a GStreamer Pipeline
 func CreatePipeline(pipelineStr string) (
-					*Pipeline,
-					error) {
+	*Pipeline,
+	error) {
 	pipeline = &Pipeline{
-		closed: 	 false,	
+		closed:      false,
 		pipeline:    unsafe.Pointer(nil),
 		pipelineStr: pipelineStr,
 		codec:       webrtc.MimeTypeH264,
 
-		clockRate:    90000,
+		clockRate: 90000,
 
 		properties: make(map[string]int),
 		AdsContext: adaptive.NewAdsContext(
-			func(bitrate int) { pipeline.SetProperty("bitrate", bitrate) }, 
-			func() 			  { pipeline.SetProperty("reset", 0) },
+			func(bitrate int) { pipeline.SetProperty("bitrate", bitrate) },
+			func() { pipeline.SetProperty("reset", 0) },
 		),
-		Multiplexer: multiplexer.NewMultiplexer("video",func() rtppay.Packetizer {
+		Multiplexer: multiplexer.NewMultiplexer("video", func() rtppay.Packetizer {
 			return h264.NewH264Payloader()
 		}),
 	}
 
-
-	
-
-
 	pipelineStrUnsafe := C.CString(pipeline.pipelineStr)
 	defer C.free(unsafe.Pointer(pipelineStrUnsafe))
 
-	var err unsafe.Pointer
-	fmt.Printf("starting video pipeline %s\n",pipelineStr)
+	err := C.CString("")
+	fmt.Printf("starting video pipeline %s\n", pipelineStr)
 	pipeline.pipeline = C.create_video_pipeline(pipelineStrUnsafe, &err)
-	err_str := ToGoString(err)
+	err_str := C.GoString(err)
+
 	if len(err_str) != 0 {
-		return nil,fmt.Errorf("failed to create pipeline %s",err_str); 
+		return nil, fmt.Errorf("failed to create pipeline %s", err_str)
 	}
 
-
-
-
-	return pipeline,nil
+	return pipeline, nil
 }
 
-//export goHandlePipelineBufferVideo
-func goHandlePipelineBufferVideo(buffer unsafe.Pointer, bufferLen C.int, duration C.int) {
-	samples := uint32(time.Duration(duration).Seconds() * pipeline.clockRate)
-	pipeline.Multiplexer.Send(buffer, uint32(bufferLen), uint32(samples) )
+func (pipeline *Pipeline) BufferVideo() {
+	var samplesInt C.int
+	var buffer unsafe.Pointer
+	size := C.PopBufferVideo(&buffer, &samplesInt)
+	samples := uint32(time.Duration(samplesInt).Seconds() * pipeline.clockRate)
+	pipeline.Multiplexer.Send(buffer, uint32(size), uint32(samples))
 }
 
 func (p *Pipeline) GetCodec() string {
@@ -101,7 +273,7 @@ func (p *Pipeline) GetCodec() string {
 func (p *Pipeline) SetProperty(name string, val int) error {
 	fmt.Printf("%s change to %d\n", name, val)
 	if p.pipeline == nil || p.properties == nil {
-		return fmt.Errorf("attemping to set property while pipeline is not running, aborting");
+		return fmt.Errorf("attemping to set property while pipeline is not running, aborting")
 	}
 
 	switch name {
@@ -121,22 +293,20 @@ func (p *Pipeline) SetProperty(name string, val int) error {
 	return nil
 }
 
-//export handleVideoStopOrError
 func handleVideoStopOrError() {
 	pipeline.Close()
 
-	var err unsafe.Pointer
-	fmt.Printf("starting video pipeline %s\n",pipeline.pipelineStr)
+	fmt.Printf("starting video pipeline %s\n", pipeline.pipelineStr)
 	pipelineStrUnsafe := C.CString(pipeline.pipelineStr)
 	defer C.free(unsafe.Pointer(pipelineStrUnsafe))
+
+	err := C.CString("")
 	pipeline.pipeline = C.create_video_pipeline(pipelineStrUnsafe, &err)
-	err_str := ToGoString(err)
+	err_str := C.GoString(err)
 	if len(err_str) != 0 {
-		fmt.Printf("failed to create pipeline %s",err_str); 
-		return 
+		fmt.Printf("failed to create pipeline %s", err_str)
+		return
 	}
-
-
 
 	pipeline.Open()
 	for key, val := range pipeline.properties {
@@ -153,18 +323,9 @@ func (p *Pipeline) Close() {
 	C.stop_video_pipeline(p.pipeline)
 }
 
-func ToGoString(str unsafe.Pointer) string {
-	if str == nil {
-		return ""
-	}
-	return string(C.GoBytes(str, C.int(C.string_get_length(str))))
-}
-
-
-
 
 func (p *Pipeline) RegisterRTPHandler(id string, fun func(pkt *rtp.Packet)) {
-	p.Multiplexer.RegisterRTPHandler(id,fun)
+	p.Multiplexer.RegisterRTPHandler(id, fun)
 }
 
 func (p *Pipeline) DeregisterRTPHandler(id string) {
