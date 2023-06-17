5a3b87c888c32487bd9ce916caed4ef276219d02
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-06-17 15:19:16 +0700

    test on multiple gpu

--- cmd/test/pipeline.go
+++ cmd/test/pipeline.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/thinkonmay/thinkremote-rtchub/listener/video"
 )
 
 const (
-	mockup_audio   = "fakesrc ! appsink name=appsink"
-	mockup_video   = "videotestsrc ! openh264enc gop-size=5 ! appsink name=appsink"
-	nvidia_default = "d3d11screencapturesrc blocksize=8192 do-timestamp=true ! capsfilter name=framerateFilter ! video/x-raw(memory:D3D11Memory),clock-rate=90000,framerate=55/1 ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=3 ! d3d11convert ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=3 ! nvd3d11h264enc bitrate=6000 gop-size=-1 preset=5 rate-control=2 strict-gop=true name=encoder repeat-sequence-header=true zero-reorder-delay=true ! video/x-h264,stream-format=(string)byte-stream,profile=(string)main ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=3 ! appsink name=appsink"
+	source = "tmd3d11screencapturesrc blocksize=8192 do-timestamp=true ! capsfilter name=framerateFilter ! video/x-raw(memory:D3D11Memory),clock-rate=90000,framerate=55/1 ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=3 ! tmd3d11convert ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=3 ! "
+	nvidia_default = source + "tmnvd3d11h264enc gop-size=-1 preset=5 rate-control=2 strict-gop=true name=encoder repeat-sequence-header=true zero-reorder-delay=true ! video/x-h264,stream-format=(string)byte-stream,profile=(string)main ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=3 ! appsink name=appsink"
+	amd_default    = source + "tmamfh264enc ! video/x-h264,stream-format=(string)byte-stream,profile=(string)main ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=3 ! appsink name=appsink"
+	intel_default  = source + "tmqsvh264enc name=encoder ! video/x-h264,stream-format=(string)byte-stream,profile=(string)main ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=3 ! appsink name=appsink"
 )
 
 
 func main() {
-	videopipeline, err := video.CreatePipeline(nvidia_default)
+
+	pipeline := ""
+	switch os.Args[1] {
+	case "amd":
+		pipeline = amd_default
+		break
+	case "nvidia":
+		pipeline = nvidia_default
+		break
+	case "intel":
+		pipeline = intel_default
+		break
+	}
+
+	videopipeline, err := video.CreatePipeline(pipeline)
+
 	if err != nil {
 		fmt.Printf("error initiate video pipeline %s\n", err.Error())
 		return
